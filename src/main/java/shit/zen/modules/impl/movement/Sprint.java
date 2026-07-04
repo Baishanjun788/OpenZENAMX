2a6a0217fe20cd3d72f7c476f74c0de44352a625
Baishanjun788 <baishanjun33@gmail.com>
2026-07-04 11:45:54 +0800

    fix8

--- src/main/java/shit/zen/modules/impl/movement/Sprint.java
+++ src/main/java/shit/zen/modules/impl/movement/Sprint.java
@@ -5,7 +5,6 @@ import shit.zen.event.impl.RotationEvent;
 import shit.zen.modules.Category;
 import shit.zen.modules.Module;
 import shit.zen.modules.impl.player.InventoryManager;
-import net.minecraft.client.KeyMapping;
 
 public class Sprint extends Module {
 
@@ -18,17 +17,16 @@ public class Sprint extends Module {
     public void onRotation(RotationEvent event) {
         if (mc.player == null) return;
         if (GuiMove.INSTANCE.isEnabled() && InventoryManager.isPerformingAction) {
-            KeyMapping.set(mc.options.keySprint.getKey(), false);
+            mc.options.keySprint.setDown(false);
             return;
         }
-        KeyMapping.set(mc.options.keySprint.getKey(), true);
+        mc.options.keySprint.setDown(true);
     }
 
     @Override
     public void onDisable() {
-        // 关闭模块时，松开疾跑键
-        if (mc.player != null) {
-            KeyMapping.set(mc.options.keySprint.getKey(), false);
+        if (mc.player != null && mc.options != null) {
+            mc.options.keySprint.setDown(false);
         }
         super.onDisable();
     }
